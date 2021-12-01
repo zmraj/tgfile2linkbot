2b2134a6612f00bf0e55144addfadc053d30d5c0
Dr DanG <75981442+zmraj@users.noreply.github.com>
2021-12-02 02:05:47 +0530

    Update telegram.py

--- tgfilestream/telegram.py
+++ tgfilestream/telegram.py
@@ -1,5 +1,5 @@
 # tgfilestream - A Telegram bot that can stream Telegram files to users over HTTP.
-# Copyright (C) 2019 Tulir Asokan
+# Copyright (C) 2019 TGFile2LinkRoBot
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
